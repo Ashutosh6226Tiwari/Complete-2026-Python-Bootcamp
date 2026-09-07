57bdf965e02487816298f7a026ed3a75b856327e
Ashutosh6226Tiwari <2k24.cs1f.2413235@gmail.com>
2026-09-07 20:18:39 +0530

    7/9

--- 11_functions/08_docstrings.py
+++ 11_functions/08_docstrings.py
@@ -1,6 +1,27 @@
+# Docstrings are used to document functions,
+# classes, and modules. In Python, they are written in triple quotes. 
+# They are accessible using the __doc__ attribute. Here's an example:
+
 def sum(a, b): 
     '''This will sum two numbers'''
     c = a + b  
     return c
 
 print(sum.__doc__)
+
+
+
+# another example 
+
+def add(a, b):
+    """
+    Returns the sum of two numbers.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int: The sum of the two numbers.
+    """
+    return a + b
